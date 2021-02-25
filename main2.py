77ff9eb9ec6a0f5fa28f38ce7b41e0947f52e8ba
streppelandres <andres.streppel@gmail.com>
2021-02-25 00:04:08 -0300

    Agrego un while true rancio, cambiar por recursividad con alguna condicion

--- main2.py
+++ main2.py
@@ -16,22 +16,29 @@ def get_all_url_players_from_page():
 DRIVER = webdriver.Chrome(executable_path=r"C:\Users\itali\Desktop\scrap_fifaindex\chromedriver_win32\chromedriver.exe")
 DRIVER.get("https://www.fifaindex.com/es/teams/fifa17_123/")
 
-print("Next page URL [" + get_next_button_url() + "]")
-for team_url in get_all_url_teams_from_page():
-    # Navigate to team page
-    print("Redirect to team [" + team_url + "]")
-    DRIVER.get(team_url)
+while True:
+    next_page_url = get_next_button_url()
+    print("Next page URL [" + next_page_url + "]")
 
-    for player_url in get_all_url_players_from_page():
-        print("Redirect to player [" + player_url + "]")
-        DRIVER.get(player_url)
+    for team_url in get_all_url_teams_from_page():
+        # Navigate to team page
+        print("Redirect to team [" + team_url + "]")
+        DRIVER.get(team_url)
 
-        elementHeader = DRIVER.find_elements_by_css_selector("h5.card-header")[0]
-        nombre = elementHeader.text.translate(str.maketrans('', '', digits)).replace('\n', ' ').replace('\r', '').strip()
-        print("Player name [" + nombre + "]")
+        for player_url in get_all_url_players_from_page():
+            print("Redirect to player [" + player_url + "]")
+            DRIVER.get(player_url)
 
-        valoraciones = [int(s) for s in re.findall(r'\b\d+\b', elementHeader.text)]
-        print("Player rate [" + ' '.join([str(elem) for elem in valoraciones])  + "]")
+            elementHeader = DRIVER.find_elements_by_css_selector("h5.card-header")[0]
+            nombre = elementHeader.text.translate(str.maketrans('', '', digits)).replace('\n', ' ').replace('\r', '').strip()
+            print("Player name [" + nombre + "]")
+
+            valoraciones = [int(s) for s in re.findall(r'\b\d+\b', elementHeader.text)]
+            print("Player rate [" + ' '.join([str(elem) for elem in valoraciones])  + "]")
+
+            print("-------------------------------------------------------------------")
+
+    DRIVER.get(next_page_url)
 
 # TODO: cuando termina el for de los teams, tiene que redireccionar a donde se haya guardado el ultimo boton
 # y repetir recursivamente todo de nuevo
