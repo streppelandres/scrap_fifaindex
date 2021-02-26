823eb97f211943153e35e49329a916d67c919cfc
streppelandres <andres.streppel@gmail.com>
2021-02-26 00:10:28 -0300

    Agrego variable opcional para indicar maximo de paginas a scrapear

--- main2.py
+++ main2.py
@@ -1,22 +1,12 @@
 from selenium import webdriver
-from string import digits
-import re
 import logging
 from datetime import datetime
+import my_functions as functions
+import csv
 
-def get_all_url_teams_from_page():
-    teams = driver.find_elements_by_css_selector("table.table-teams tbody tr td[data-title='Nombre'] a.link-team")
-    return [one_team.get_attribute('href') for one_team in teams]
-
-def get_next_button_url():
-    return driver.find_element_by_css_selector("nav ul.pagination li.ml-auto a.btn.btn-light").get_attribute('href')
-
-def get_all_url_players_from_page():
-    players = driver.find_elements_by_css_selector("table.table-players tbody td[data-title='Nombre'] a.link-player")
-    return [one_player.get_attribute('href') for one_player in players]
-
-def get_team_name_from_team_page():
-    return driver.find_element_by_css_selector('div.col-lg-8 nav ol.breadcrumb.bg-primary li.breadcrumb-item.active').text
+# flag para saber si es la primer iteraccion
+flagFirstTime = True
+LIMITE_CANTIDAD_PAGINAS = 0 # indicar la cantidad de paginas a scrapear, si es [0] va ser "infinito"
 
 # log config TODO: Meterlos en una carpeta
 logging.basicConfig(filename=datetime.today().strftime('%Y%m%d') + '_logging.log', encoding='utf-8', format='%(asctime)s %(message)s', level=logging.INFO)
@@ -25,25 +15,36 @@ logging.basicConfig(filename=datetime.today().strftime('%Y%m%d') + '_logging.log
 driver = webdriver.Chrome(executable_path=r"C:\chromedriver_win32\chromedriver.exe")
 driver.get("https://www.fifaindex.com/es/teams/fifa17_123/") # Navigate to the frist team's page
 
-# TODO: cambiar el while-true por el max numero de paginas, lo tenias anotado por ahi
+i = 0
 while True:
-    next_page_url = get_next_button_url()
+    next_page_url = functions.get_next_button_url(driver)
 
     # por cada equipo de esa pagina
-    for team_url in get_all_url_teams_from_page():
+    for team_url in functions.get_all_url_teams_from_page(driver):
         logging.info("Redireccionando al equipo [" + team_url + "]")
         driver.get(team_url) # Navigate to team page
-        team_name = get_team_name_from_team_page()
 
-        # por cada jugador de ese equipo
-        for player_url in get_all_url_players_from_page():
-            driver.get(player_url) # Navigate to player page
-            logging.info("Redireccionando al jugador [" + player_url + "]")
+        team_name = functions.get_team_name_from_team_page(driver)
 
-            elementHeader = driver.find_elements_by_css_selector("h5.card-header")[0]
-            nombre = elementHeader.text.translate(str.maketrans('', '', digits)).replace('\n', ' ').replace('\r', '').strip()
-            valoraciones = [int(s) for s in re.findall(r'\b\d+\b', elementHeader.text)]
+        # abro el csv
+        with open('player_list.csv', 'a', newline='', encoding="utf-8") as file:
+            writer = csv.writer(file)
 
-            print("| " + ' '.join([str(elem) for elem in valoraciones]) + "\t| " + team_name + "\t| " + nombre)
+            if(flagFirstTime):
+                writer.writerow(functions.get_csv_header())
+                flagFirstTime = False
 
+            # por cada jugador de ese equipo
+            for player_url in functions.get_all_url_players_from_page(driver):
+                logging.info("Redireccionando al jugador [" + player_url + "]")
+                driver.get(player_url) # navigate to player page
+
+                # scrapeo el player y lo guardo en el csv
+                writer.writerow(functions.do_scrap_player(driver, team_name)) 
+
+    if((LIMITE_CANTIDAD_PAGINAS-1) == i):
+        break
+    
     driver.get(next_page_url) # al finalizar esta pagina de equipos, voy a la siguiente
+
+    i+=1
---
+++ my_functions.py
@@ -0,0 +1,39 @@
+from string import digits
+import re
+
+# algunos metodos
+def get_all_url_teams_from_page(driver):
+    teams = driver.find_elements_by_css_selector("table.table-teams tbody tr td[data-title='Nombre'] a.link-team")
+    return [one_team.get_attribute('href') for one_team in teams]
+
+def get_next_button_url(driver):
+    return driver.find_element_by_css_selector("nav ul.pagination li.ml-auto a.btn.btn-light").get_attribute('href')
+
+def get_all_url_players_from_page(driver):
+    players = driver.find_elements_by_css_selector("table.table-players tbody td[data-title='Nombre'] a.link-player")
+    return [one_player.get_attribute('href') for one_player in players]
+
+def get_team_name_from_team_page(driver):
+    return driver.find_element_by_css_selector('div.col-lg-8 nav ol.breadcrumb.bg-primary li.breadcrumb-item.active').text
+
+def get_csv_header():
+    return ["Equipo", "Nombre", "V1", "V2"]
+
+def do_scrap_player(driver, team_name):
+    player_data_array = []
+
+    # equipo
+    player_data_array.append(team_name)
+
+    # nombre
+    elementHeader = driver.find_elements_by_css_selector("h5.card-header")[0]
+    player_data_array.append(
+        elementHeader.text.translate(str.maketrans('', '', digits)).replace('\n', ' ').replace('\r', '').strip()
+    )
+    # valoraciones
+    valoraciones = [int(s) for s in re.findall(r'\b\d+\b', elementHeader.text)]
+    player_data_array.append(str(valoraciones[0]))
+    player_data_array.append(str(valoraciones[1]))
+
+
+    return player_data_array
