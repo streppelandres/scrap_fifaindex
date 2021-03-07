cad1ec7bbcaee8a5a29356a6fb9f39ef1e5a30a6
streppelandres <andres.streppel@gmai.com>
2021-03-07 20:36:17 -0300

    Traigo las equipaciones del equipo

--- src/team_functions.py
+++ src/team_functions.py
@@ -1,24 +1,23 @@
 import urllib.request
+import os
 
 def get_and_download_team_img(driver, team_id):
     img_src = driver.find_element_by_css_selector("div.row.pt-3 div.col-sm-6.col-md-7 div.d-flex.mb-3.align-items-center div.align-self-center img.team.size-10").get_attribute("src")
     
-    # https://www.fifaindex.com/static/FIFA17/images/crest/10/light/21.webp
-    
-    # img_src = img_src.replace(".webp", ".png") #img_src[0:len(img_src)-4] + "png" # Cambio el formato a png
-    print("-----------> " + os.getcwd())
-    urllib.request.urlretrieve(img_src, "img/team/" + team_id + "/" + team_id + ".png")
+    path = os.getcwd() + "/img/team/" + team_id
+    os.makedirs(path, exist_ok=True)
+
+    urllib.request.urlretrieve(img_src, path + "/" + team_id + ".png")
 
 def get_and_download_team_kits(driver, team_id):
-    kit_web_element_list = driver.find_element_by_css_selector("div.col-12.col-lg-7 div.card.mb-5 div.card-body div.row div.col-6.text-center")
+    kit_web_element_list = driver.find_elements_by_css_selector("div.col-12.col-lg-7 div.card.mb-5 div.card-body div.row div.col-6.text-center")
 
     # por cada camiseta encontrada
     for we_kit in kit_web_element_list:
         we_img_src = we_kit.find_element_by_css_selector("img.kit.img-fluid").get_attribute("src")
-        we_p = we_kit.find_element_by_css_selector("p")
+        we_p = we_kit.find_element_by_css_selector("p").text
 
         urllib.request.urlretrieve(we_img_src, "img/team/" + team_id + "/" + team_id + "_" + we_p + ".png")
-
 
 def get_csv_header():
     h_row = []
