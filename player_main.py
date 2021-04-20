59f9f023bda974bec956bcffa2cf9ac8c865fa2a
streppelandres <andres.streppel@gmai.com>
2021-04-20 01:05:37 -0300

    Agrego hardcodeo rancio para que continue en cierto team/player

--- player_main.py
+++ player_main.py
@@ -13,6 +13,9 @@ g_functions.create_directories(os.getcwd())
 DATA_VERSION_URL = "fifa17_123" # url version de la data
 DATA_VERSION_DATE = "27-03-2017" # fecha de la data (hardcodeado a pedido de J)
 DATE_TODAY = datetime.today().strftime('%Y%m%d')
+hardCodeUrl = True
+hardCodeUrlTeam = "https://www.fifaindex.com/es/team/111706/godoy-cruz/fifa17_123/"
+hardCodeUrlPlayer = "https://www.fifaindex.com/es/player/220059/diego-poyet/fifa17_123/"
 
 # log config
 logging.basicConfig(filename= "logs/" + DATE_TODAY + '_player_logging.log', encoding='utf-8', format='%(asctime)s %(message)s', level=logging.INFO)
@@ -26,7 +29,7 @@ opener=urllib.request.build_opener()
 opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
 urllib.request.install_opener(opener)
 
-for i in range(1, g_functions.get_cant_pages(driver) + 1):
+for i in range(11, g_functions.get_cant_pages(driver) + 1):
     next_page = "https://www.fifaindex.com/es/teams/"+ DATA_VERSION_URL +"/?page=" + str(i)
     logging.info("Redireccionando a la pagina [" + next_page + "]")
     driver.get(next_page) # Al finalizar esta pagina de equipos, voy a la siguiente
@@ -42,7 +45,14 @@ for i in range(1, g_functions.get_cant_pages(driver) + 1):
             break
 
     # por cada equipo de esa pagina
-    for team_url in g_functions.get_all_url_teams_from_page(driver):
+    urls_team_list = g_functions.get_all_url_teams_from_page(driver)
+
+    if (hardCodeUrl):
+        # me agarro apartir del equipo hardcodeado
+        urls_team_list = urls_team_list[urls_team_list.index(hardCodeUrlTeam):len(urls_team_list)]
+
+    for team_url in urls_team_list:
+
         logging.info("Redireccionando al equipo [" + team_url + "]")
         driver.get(team_url) # Navigate to team page
 
@@ -53,15 +63,22 @@ for i in range(1, g_functions.get_cant_pages(driver) + 1):
         with open('data/' + DATE_TODAY + '_player_list.csv', 'a', newline='', encoding="utf-8") as file:
             writer = csv.writer(file, quotechar='&') # el quotechar tiene que ser algo que no se use para nada
 
-            if(i == 1):
+            #if(i == 1):
                 # si es la primera vez agrego el header de las columnas
-                writer.writerow(functions.get_csv_header())
+                #writer.writerow(functions.get_csv_header())
 
             # por cada jugador de ese equipo
-            for player_url in functions.get_all_url_players_from_page(driver):
+            urls_player_list = functions.get_all_url_players_from_page(driver)
+
+            if (hardCodeUrl):
+                urls_player_list = urls_player_list[urls_player_list.index(hardCodeUrlPlayer):len(urls_player_list)]
+                hardCodeUrl = False
+
+            for player_url in urls_player_list:
+                
                 logging.info("Redireccionando al jugador [" + player_url + "]")
                 driver.get(player_url) # navigate to player page
-
+                    
                 while True: # TODO: Extraer esto en una funcion
                     try:
                         # Si encuentra el elemento significa que hay error, sigo dentro del while
@@ -74,11 +91,7 @@ for i in range(1, g_functions.get_cant_pages(driver) + 1):
                         break
 
                 # scrapeo el player y lo guardo en el csv
-                writer.writerow(
-                    functions.do_scrap_player(
-                            driver, team_name, team_id, functions.get_player_id_from_url(player_url), DATA_VERSION_DATE
-                        )
-                    )
+                writer.writerow(functions.do_scrap_player(driver, team_name, team_id, functions.get_player_id_from_url(player_url), DATA_VERSION_DATE))
     
 else:
     logging.info("Se termino de scrapear los equipos, se va cerrar el driver")
